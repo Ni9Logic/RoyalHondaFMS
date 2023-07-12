0a86d4bd345b77c12fedbff7db0df9c6b02c6425
= <=>
2023-07-12 19:51:05 +0500

    Working on register

---
+++ app/api/auth/[...nextauth]/route.ts
@@ -0,0 +1,53 @@
+import bcrypt from 'bcrypt';
+import NextAuth, { AuthOptions } from 'next-auth';
+import CredentialsProvider from 'next-auth/providers/credentials';
+import { PrismaAdapter } from '@next-auth/prisma-adapter';
+
+import prisma from '../../../lib/prismadb';
+
+const authOptions: AuthOptions = {
+    adapter: PrismaAdapter(prisma),
+    providers: [
+        CredentialsProvider({
+            credentials: {
+                email: { label: 'Email', type: 'text' },
+                password: { label: 'Password', type: 'password' },
+            },
+            async authorize(credentials) {
+                if (!credentials?.email || !credentials?.password) {
+                    throw new Error('Invalid Credentials')
+                }
+
+                const user: any = prisma.webUser.findUnique({
+                    where: {
+                        email: credentials.email
+                    }
+                })
+
+                // Now checking if the user exists or not
+                if (!user) {
+                    throw new Error('Invalid Credentials')
+                }
+
+                // Now comparing the password of the user
+                const isCorrect = await bcrypt.compare(credentials.password, user?.hashedPassword);
+
+                if (!isCorrect) {
+                    throw new Error('Invalid Credentials')
+                }
+
+
+                // If all of the above cases are true then lets return user
+                return user;
+            }
+        }),
+    ],
+    debug: process.env.NODE_ENV === 'development',
+    session: {
+        strategy: "jwt",
+    },
+    secret: process.env.SECRET
+};
+
+const handler = NextAuth(authOptions);
+export { handler as GET, handler as POST };
