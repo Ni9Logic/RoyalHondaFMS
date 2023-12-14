1659be6a240fc7f1e82930d1f77cd4934da6f820
Hassan Rehman <rehmanhassan117@gmail.com>
2023-12-14 19:21:53 +0500

    changes

--- app/users/UserMenu.tsx
+++ app/users/UserMenu.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
-import Link from 'next/link';
 import getCurrentUser from '../actions/getCurrentUser';
 
 export default async function UserMenu() {
     const user = await getCurrentUser();
+
     return (
         <section className="flex flex-col text-gray-600 body-font h-[75vh] justify-center items-center">
             {/* Implementation of how the user will update, view weightage and add assignments and modify the subjects */}
