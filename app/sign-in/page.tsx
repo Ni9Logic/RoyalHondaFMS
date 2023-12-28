776cc41b4d241d8ee2e6118bca30a75e60d375c3
Hassan Rehman <rehmanhassan117@gmail.com>
2023-12-28 13:43:03 +0500

    Add Button component and Loader to sign-in page

--- app/sign-in/page.tsx
+++ app/sign-in/page.tsx
@@ -6,6 +6,8 @@ import { signIn, useSession } from "next-auth/react"
 import { FieldValues, SubmitHandler, useForm } from "react-hook-form";
 import { toast } from 'react-hot-toast';
 import { useRouter } from 'next/navigation';
+import { Button } from '@/components/ui/button';
+import Loader from '../components/ui/loader';
 
 export default function Page() {
     const router = useRouter();
@@ -60,7 +62,10 @@ export default function Page() {
                         <label htmlFor="helper-text" className="flex flex-col my-2 mb-2 text-sm font-medium text-gray-900 dark:text-white">Your Password</label>
                         <input type="password" {...register('password')} id="helper-text" aria-describedby="helper-text-explanation" className="w-[500px] bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="***********" />
                     </div>
-                    <button type="submit" className={`w-full gap-10 my-10 px-20 text-white bg-[#78c3fc] hover:bg-[#78c3fc] focus:ring-4 focus:ring-blue-300 font-medium rounded-lg ${isLoading ? "cursor-not-allowed opacity-25" : ""} text-sm py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800`} disabled={isLoading}>Login</button>
+                    <Button className='mt-2 w-full flex flex-row gap-1' disabled={isLoading}>
+                        Submit
+                        <Loader isLoading={isLoading}/>
+                    </Button>
                 </form>
             </div>
         </>
